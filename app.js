d8bb73efd54f27b9841918cbea8598b0fa102be8
aprin418 <aprin418@gmail.com>
2021-04-16 16:31:58 -0400

    update doms.js and app.js

---
+++ app.js
@@ -0,0 +1,14 @@
+const newTitle = document.querySelector("title");
+newTitle.textContent = "Coding Bootcamps, Data Science, UX, Business | General Assembly";
+
+const newH1 = document.querySelector("#title");
+newH1.textContent = "General Assembly";
+
+const pOne = document.querySelector("#one");
+pOne.textContent = "We Are the future of work."
+
+const pTwo = document.querySelector("#two");
+pTwo.textContent = "Individuals"
+
+const pThree = document.querySelector(".three");
+pThree.textContent = "Companies";
--- doms.js
+++ doms.js
@@ -31,3 +31,66 @@ console.log(imgDiv);
 
 const divUlLi = document.querySelector('.listItem');
 console.log(divUlLi);
+
+// getElementByClassName()
+//.quesrySelectorAll()
+
+const containerTwoList = document.getElementsByClassName('container-2'); //html collection
+console.log(containerTwoList.item(3));
+
+const containerTwoListTwo = document.querySelectorAll('.container-2'); //node list
+console.log(containerTwoListTwo);
+
+const containerTwoArray = Array.from(containerTwoList); //makes this an array
+console.log(containerTwoArray);
+
+for (let i = 0; i < containerTwoArray.length; i++) {
+    let element = containerTwoArray[i];
+    console.log(element);
+};
+
+const thirdElement = containerTwoArray[2];
+console.log(thirdElement);
+
+const pOne2 = document.querySelector('#one');
+console.log(pOne2);
+console.log(pOne2.textContent);
+pOne2.textContent="GoodBye!";
+console.log(pOne2.textContent);
+pOne.textContent="French Vanilla";
+console.log(pOne.textContent);
+pTwo.textContent="rocky Road";
+console.log(pTwo.textContent);
+pThree.textContent="Chocolate, peanut butter, cookie dough";
+console.log(pThree.textContent);
+imgDiv.scr="https://i.imgur.com/r1w3fYq.jpg";
+imgDiv.alt="scoop there it is";
+console.log(imgDiv.textContent);
+divUlLi.textContent="scoop there it is";
+console.log(divUlLi.textContent);
+// iceCreamImg = document.querySelector("image");
+// iceCreamImg.scr = "https://i.imgur.com/r1w3fYq.jpg";
+// console.log(iceCreamImg.src);
+
+//creating elements
+const newDiv = document.createElement('div');
+console.log(newDiv);
+
+newDiv.textContent = 'New Element';
+// append something to page
+//1. grab existing element off the page
+//2. append new element to the page
+const body = document.querySelector('body');
+body.append(newDiv);
+
+const newImg = document.createElement("img");
+// newImg = document.querySelector('body');
+newImg.src = "https://i.imgur.com/r1w3fYq.jpg";
+body.append(newImg);
+
+function addElement (ele) {
+    const newEle = document.createElement(ele);
+    body.append(newEle);
+};
+
+
